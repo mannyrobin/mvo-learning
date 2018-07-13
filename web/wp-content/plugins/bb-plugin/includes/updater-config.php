a137b6f3e80ca1c6d7ee33e8dda50c8301691c38
mannyrobin <69haiqian@gmail.com>
2018-07-13 17:12:49 -0500

    Update WP core and plugins

--- web/wp-content/plugins/bb-plugin/includes/updater-config.php
+++ web/wp-content/plugins/bb-plugin/includes/updater-config.php
@@ -3,7 +3,7 @@
 if ( class_exists( 'FLUpdater' ) ) {
 	FLUpdater::add_product(array(
 		'name'      => 'Beaver Builder Plugin (Pro Version)',
-		'version'   => '2.1.3.4',
+		'version'   => '2.1.3.5',
 		'slug'      => 'bb-plugin',
 		'type'      => 'plugin',
 	));
