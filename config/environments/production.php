7ca79ebf7aa008b45235bc230b8094cf072d7de7
mannyrobin <69haiqian@gmail.com>
2018-08-17 20:24:41 -0500

    update SSL redirect with live URL

--- config/environments/production.php
+++ config/environments/production.php
@@ -10,7 +10,7 @@ if (!empty(env('PANTHEON_ENVIRONMENT')) && php_sapi_name() != 'cli') {
   // Redirect to https://$primary_domain in the Live environment
   if (env('PANTHEON_ENVIRONMENT') === 'live') {
     /** Replace www.example.com with your registered domain name */
-    $primary_domain = 'live-mvo-learning.pantheonsite.io';
+    $primary_domain = 'ministryvillageelc.org';
   }
   else {
     // Redirect to HTTPS on every Pantheon environment.
