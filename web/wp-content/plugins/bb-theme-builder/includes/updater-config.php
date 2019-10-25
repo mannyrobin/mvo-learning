ac238fc2b265aac75860ac34df5dad2085f310ca
mannyrobin <69haiqian@gmail.com>
2019-10-25 09:15:55 +0000

    update wp core and plugins

--- web/wp-content/plugins/bb-theme-builder/includes/updater-config.php
+++ web/wp-content/plugins/bb-theme-builder/includes/updater-config.php
@@ -3,7 +3,7 @@
 if ( class_exists( 'FLUpdater' ) ) {
 	FLUpdater::add_product( array(
 		'name'    => 'Beaver Themer',
-		'version' => '1.2.4.2',
+		'version' => '1.2.4.3',
 		'slug'    => 'bb-theme-builder',
 		'type'    => 'plugin',
 	) );
