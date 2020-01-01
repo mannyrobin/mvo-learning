259eaee3e2466f0b85da844e8d2414ba9551fb09
mannyrobin <69haiqian@gmail.com>
2020-01-01 14:11:18 +0600

    HTTPS/HSTS Setup

--- config/environments/production.php
+++ config/environments/production.php
@@ -6,34 +6,17 @@ define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
 
+
 if (!empty(env('PANTHEON_ENVIRONMENT')) && php_sapi_name() != 'cli') {
-  // Redirect to https://$primary_domain in the Live environment
-  if (env('PANTHEON_ENVIRONMENT') === 'live') {
-    /** Replace www.example.com with your registered domain name */
-    $primary_domain = 'ministryvillageelc.org';
-  }
-  else {
-    // Redirect to HTTPS on every Pantheon environment.
-    $primary_domain = $_SERVER['HTTP_HOST'];
-  }
-  
-  if ( $_SERVER['HTTP_HOST'] == 'refer.live-drhackel.pantheonsite.io' ) {
-		header('HTTP/1.0 301 Moved Permanehtly');
-		header('Location: https://' . $primary_domain . '/refer');
-		exit();
+	if (env('PANTHEON_ENVIRONMENT') === 'live') {
+		$primary_domain = 'ministryvillageelc.org';
+	} else {
+		$primary_domain = $_SERVER['HTTP_HOST'];
 	}
 
-  if ($_SERVER['HTTP_HOST'] != $primary_domain
-      || !isset($_SERVER['HTTP_USER_AGENT_HTTPS'])
-      || $_SERVER['HTTP_USER_AGENT_HTTPS'] != 'ON' ) {
-
-    # Name transaction "redirect" in New Relic for improved reporting (optional)
-    if (extension_loaded('newrelic')) {
-      newrelic_name_transaction("redirect");
-    }
-
-    header('HTTP/1.0 301 Moved Permanently');
-    header('Location: https://'. $primary_domain . $_SERVER['REQUEST_URI']);
-    exit();
-  }
+	if ($_SERVER['HTTP_HOST'] != $primary_domain) {
+		header('HTTP/1.0 301 Moved Permanently');
+		header('Location: https://' . $primary_domain . $_SERVER['REQUEST_URI']);
+		exit();
+	}
 }
